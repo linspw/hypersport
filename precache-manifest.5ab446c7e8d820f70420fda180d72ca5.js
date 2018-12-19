52106d579a28f1ce69928ac1a7b815d7ef632c5f
linspw <linspw@hotmail.com>
2018-12-18 22:40:29 -0200

    Updates

---
+++ precache-manifest.5ab446c7e8d820f70420fda180d72ca5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "56c4cb26fd6a48b9c0ebcc07b376ee38",
+    "url": "/hypersport/static/media/Lato-Italic.56c4cb26.ttf"
+  },
+  {
+    "revision": "0f8797c2975f2feb9d4e",
+    "url": "/hypersport/static/css/main.bcd0f73e.chunk.css"
+  },
+  {
+    "revision": "1ba4767ee37aab7e8d34fc339c3538cc",
+    "url": "/hypersport/static/media/Lato-BoldItalic.1ba4767e.ttf"
+  },
+  {
+    "revision": "d9392b0a3dae93192bc4",
+    "url": "/hypersport/static/js/1.d9392b0a.chunk.js"
+  },
+  {
+    "revision": "7a457fcf2eb88f4d6b9c",
+    "url": "/hypersport/static/js/runtime~main.7a457fcf.js"
+  },
+  {
+    "revision": "0ec238109d29cf8b027324dcd6f8d04b",
+    "url": "/hypersport/static/media/Phoenixians_Italic.0ec23810.otf"
+  },
+  {
+    "revision": "0f8797c2975f2feb9d4e",
+    "url": "/hypersport/static/js/main.0f8797c2.chunk.js"
+  },
+  {
+    "revision": "7f690e503a254e0b8349aec0177e07aa",
+    "url": "/hypersport/static/media/Lato-Regular.7f690e50.ttf"
+  },
+  {
+    "revision": "0cac469efe377d5e76cc11d53c6c1e13",
+    "url": "/hypersport/static/media/Orbitron-Regular.0cac469e.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/hypersport/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "d9392b0a3dae93192bc4",
+    "url": "/hypersport/static/css/1.8e2722f7.chunk.css"
+  },
+  {
+    "revision": "a4aa96a6821e1bde0836cdd067b5a2d1",
+    "url": "/hypersport/index.html"
+  }
+];
